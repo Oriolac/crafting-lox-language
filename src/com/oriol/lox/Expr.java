289d31fa3c7fc3b406f5b00cda3ae3f6024aadda
Oriol <oriolalascercos@gmail.com>
2019-12-13 16:58:39 +0100

    Tot fet fins defineType. Falta compilar

---
+++ src/com/oriol/lox/Expr.java
@@ -0,0 +1,20 @@
+package com.oriol.lox;
+
+
+public abstract class Expr {
+
+    static class Binary extends Expr {
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+    }
+
+}
